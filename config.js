b093159bd35ad9872b7edac9e883ffb3b7461a04
gaciasreygodwen-oss <gaciasreygodwen@gmail.com>
2026-09-04 11:44:00 +0800

    new file

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const SUPABASE_URL = 'YOUR_SUPABASE_URL_HERE';
+const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
