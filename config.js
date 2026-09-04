3f08f88c9f087554c951150cc683fe207a041244
emerald2026 <emerald11tvl@gmail.com>
2026-09-04 17:12:17 +0800

    rename api

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const SUPABASE_URL = "https://tlwxoifwjbjebetonlgb.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_MrQpBMvzZg6hEs94JI_LmA_TLO9x4UF";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsd3hvaWZ3amJqZWJldG9ubGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MDAyOTUsImV4cCI6MjEwNDA3NjI5NX0.iZRENiJByVri8g1cJi84PS9s7h8fxZWKa_qbolJeK6U";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
